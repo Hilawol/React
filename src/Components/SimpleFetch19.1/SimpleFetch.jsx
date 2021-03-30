bf0d0587b661879306bbb8c1d99a33e601db1f8b
Hila Kershenovich <hilawol@gmail.com>
2021-03-30 11:24:39 +0300

    remove console.log

--- src/Components/SimpleFetch19.1/SimpleFetch.jsx
+++ src/Components/SimpleFetch19.1/SimpleFetch.jsx
@@ -4,12 +4,10 @@ import axios from 'axios'
 function SimpleFetch() {
 
   const [movies, setMovies] = useState([]);
-  console.log(movies);
   useEffect(() => {
 
     const getMovies = async () => {
       const { data } = await axios.get('https://swapi.dev/api/films/');
-      console.log(data.results);
       setMovies(data.results.map(m => { return { title: m.title, director: m.director } }));
     }
     getMovies();
